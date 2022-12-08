8386d615e7f66123fadbd8ad68be6202e0cfec5f
LinneaGab <118212610+LinneaGab@users.noreply.github.com>
2022-12-08 17:36:38 +0100

    fixed

---
+++ home.js
@@ -0,0 +1,14 @@
+const menu = document.getElementById('menulogo')
+const dropdown = document.getElementById('dropdownmenu')
+
+dropdown.style.display = 'none';
+
+menu.addEventListener('click', () => {
+    if (dropdown.style.display === 'none') {
+      dropdown.style.display = 'block';
+    } else {
+      dropdown.style.display = 'none';
+    }
+  });
+
+  
---
+++ orderpage.js
@@ -0,0 +1,8 @@
+const button1 = document.getElementById('cardbutton')
+const button2 = document.getElementById('completebutton')
+
+button2.style.display = 'none';
+
+button1.addEventListener('click', function() {
+  button2.style.display = 'inline-block';
+});
